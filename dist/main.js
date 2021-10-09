53c7759ccbf4d53146ff281c92e49ccee86bb9a5
ngvuong <vminguyen@ucdavis.edu>
2021-10-09 04:15:10 -0700

    Update parse weather data

--- dist/main.js
+++ dist/main.js
@@ -6,14 +6,18 @@ var __webpack_exports__ = {};
 async function fetchWeather(location = "London") {
   const city = location || "London";
 
-  const baseUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f2770452bcb842b20425a6d7a6413b9e`;
+  const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f2770452bcb842b20425a6d7a6413b9e`;
 
-  const weatherData = await fetch(baseUrl, { mode: "cors" });
+  const response = await fetch(baseUrl);
+  const weatherData = await response.json();
   console.log(weatherData);
+  return weatherData;
 }
 
-fetchWeather("Ninh Binh");
+fetchWeather("Vancouver");
+
+function parseWeather(data) {}
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7O0FBRUEsK0RBQStELEtBQUs7O0FBRXBFLDZDQUE2QyxjQUFjO0FBQzNEO0FBQ0E7O0FBRUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWF0aGVyYXBwLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImFzeW5jIGZ1bmN0aW9uIGZldGNoV2VhdGhlcihsb2NhdGlvbiA9IFwiTG9uZG9uXCIpIHtcbiAgY29uc3QgY2l0eSA9IGxvY2F0aW9uIHx8IFwiTG9uZG9uXCI7XG5cbiAgY29uc3QgYmFzZVVybCA9IGBhcGkub3BlbndlYXRoZXJtYXAub3JnL2RhdGEvMi41L3dlYXRoZXI/cT0ke2NpdHl9JnVuaXRzPWltcGVyaWFsJmFwcGlkPWYyNzcwNDUyYmNiODQyYjIwNDI1YTZkN2E2NDEzYjllYDtcblxuICBjb25zdCB3ZWF0aGVyRGF0YSA9IGF3YWl0IGZldGNoKGJhc2VVcmwsIHsgbW9kZTogXCJjb3JzXCIgfSk7XG4gIGNvbnNvbGUubG9nKHdlYXRoZXJEYXRhKTtcbn1cblxuZmV0Y2hXZWF0aGVyKFwiTmluaCBCaW5oXCIpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7O0FBRUEsdUVBQXVFLEtBQUs7O0FBRTVFO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7O0FBRUE7O0FBRUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWF0aGVyYXBwLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImFzeW5jIGZ1bmN0aW9uIGZldGNoV2VhdGhlcihsb2NhdGlvbiA9IFwiTG9uZG9uXCIpIHtcbiAgY29uc3QgY2l0eSA9IGxvY2F0aW9uIHx8IFwiTG9uZG9uXCI7XG5cbiAgY29uc3QgYmFzZVVybCA9IGBodHRwczovL2FwaS5vcGVud2VhdGhlcm1hcC5vcmcvZGF0YS8yLjUvd2VhdGhlcj9xPSR7Y2l0eX0mdW5pdHM9aW1wZXJpYWwmYXBwaWQ9ZjI3NzA0NTJiY2I4NDJiMjA0MjVhNmQ3YTY0MTNiOWVgO1xuXG4gIGNvbnN0IHJlc3BvbnNlID0gYXdhaXQgZmV0Y2goYmFzZVVybCk7XG4gIGNvbnN0IHdlYXRoZXJEYXRhID0gYXdhaXQgcmVzcG9uc2UuanNvbigpO1xuICBjb25zb2xlLmxvZyh3ZWF0aGVyRGF0YSk7XG4gIHJldHVybiB3ZWF0aGVyRGF0YTtcbn1cblxuZmV0Y2hXZWF0aGVyKFwiVmFuY291dmVyXCIpO1xuXG5mdW5jdGlvbiBwYXJzZVdlYXRoZXIoZGF0YSkge31cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 async function fetchWeather(location = "London") {
   const city = location || "London";
 
-  const baseUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f2770452bcb842b20425a6d7a6413b9e`;
+  const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f2770452bcb842b20425a6d7a6413b9e`;
 
-  const weatherData = await fetch(baseUrl, { mode: "cors" });
+  const response = await fetch(baseUrl);
+  const weatherData = await response.json();
   console.log(weatherData);
+  return weatherData;
 }
 
-fetchWeather("Ninh Binh");
+// fetchWeather("Vancouver");
+
+function parseWeather(data) {
+  const city = data.name;
+  const temp = data.main.temp;
+  const maxTemp = data.main.temp_max;
+  const minTemp = data.main.temp_min;
+}
