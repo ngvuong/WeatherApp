ad0911c22a549325133b513b29cda335dee9fbfb
ngvuong <vminguyen@ucdavis.edu>
2021-10-11 23:54:30 -0700

    Update forecast data parsing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import { windDirection } from "./windDir";
+import { format } from "date-fns";
 
 // console.log(windDirection(309));
 
-async function fetchWeather(location = "London") {
-  const city = location || "London";
-
+async function fetchWeather(city = "London") {
   const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f2770452bcb842b20425a6d7a6413b9e`;
 
   try {
@@ -26,18 +25,25 @@ async function fetchWeather(location = "London") {
 // fetchWeather("Santa Rosa, US");
 
 function parseWeather(data) {
-  const city = data.name;
-  const country = data.sys.country;
-  const temp = data.main.temp;
-  const maxTemp = data.main.temp_max;
-  const minTemp = data.main.temp_min;
-  const description = data.weather[0].description;
+  const city = data.dayWeather.name;
+  const country = data.dayWeather.sys.country;
+  const temp = data.dayWeather.main.temp;
+  const maxTemp = data.dayWeather.main.temp_max;
+  const minTemp = data.dayWeather.main.temp_min;
+  const description = data.dayWeather.weather[0].description;
+  const forecast = data.weekWeather.daily.reduce((acc, day) => {
+    const obj = { day: format(day.dt * 1000, "EEEE"), temp: day.temp };
+    // obj[format(day.dt * 1000, "yyyy-MM-dd")] = day.temp;
+    acc.push(obj);
+    return acc;
+  }, []);
 
-  return { city, country, temp, maxTemp, minTemp, description };
+  return { city, country, temp, maxTemp, minTemp, description, forecast };
 }
 
 (async function displayWeather() {
-  const display = document.querySelector(".display");
+  const dayDisplay = document.querySelector(".day-display");
+  const weekDisplay = document.querySelector(".week-display");
   const search = document.querySelector(".search");
 
   search.addEventListener("submit", async (e) => {
@@ -47,18 +53,20 @@ function parseWeather(data) {
   });
 
   function getData(city) {
-    const data = fetchWeather(city).then((response) =>
-      parseWeather(response.dayWeather)
-    );
+    const data = fetchWeather(city).then((response) => parseWeather(response));
     return data;
   }
 
   setDisplay(await getData("London"));
 
   function setDisplay(data) {
-    display.innerHTML = `${data.city}, ${data.country}
+    console.log(data.forecast);
+
+    dayDisplay.innerHTML = `${data.city}, ${data.country}
     Temperature: ${data.temp}
     Low: ${data.minTemp} High: ${data.maxTemp}
     ${data.description}`;
+
+    const forecast = data.forecast;
   }
 })();
