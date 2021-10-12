1c5a59e158b5b50234c66fc6c6b20a3fccfd7728
ngvuong <vminguyen@ucdavis.edu>
2021-10-11 20:35:36 -0700

    Uodate display weather

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { windDirection } from "./windDir";
 
-console.log(windDirection(256));
+// console.log(windDirection(309));
 
 async function fetchWeather(location = "London") {
   const city = location || "London";
@@ -13,11 +13,33 @@ async function fetchWeather(location = "London") {
   return weatherData;
 }
 
-fetchWeather("San Francisco, CA, US");
+// fetchWeather("Santa Rosa, US");
 
 function parseWeather(data) {
   const city = data.name;
+  const country = data.sys.country;
   const temp = data.main.temp;
   const maxTemp = data.main.temp_max;
   const minTemp = data.main.temp_min;
+  const description = data.weather[0].description;
+  return { city, country, temp, maxTemp, minTemp, description };
 }
+
+(async function displayWeather() {
+  const display = document.querySelector(".display");
+  const search = document.querySelector(".search");
+
+  let city;
+  search.addEventListener("submit", (e) => {
+    e.preventDefault();
+    city = search.city.value;
+    // displayWeather();
+  });
+  const response = await fetchWeather(city);
+  const weatherData = parseWeather(response);
+
+  display.innerHTML = `${weatherData.city}, ${weatherData.country}
+  Temperature: ${weatherData.temp}
+  Low: ${weatherData.minTemp} High: ${weatherData.maxTemp}
+  ${weatherData.description}`;
+})();
